2e0511927ed1c7e0760c70b101bce9e620405612
mujtabachandio <mujtabachandio384@gmail.com>
2025-05-03 03:58:01 +0500

    react_resume_build

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -42,17 +42,6 @@ export default function Footer() {
                 align={{ base: 'center', md: 'center' }}>
                 <Image style={{ height: '44px' }} src={logo} alt="logo" />
                 <Text>© 2023 Resume Builder, All rights reserved</Text>
-                <Stack direction={'row'} spacing={6}>
-                    <SocialButton label={'Github'} href={'https://github.com/imhardikdesai'}>
-                        <FaGithub />
-                    </SocialButton>
-                    <SocialButton label={'Snapchat'} href={'https://twitter.com/imhardikdesai'}>
-                        <FaSnapchat />
-                    </SocialButton>
-                    <SocialButton label={'Instagram'} href={'https://instagram.com/imhardikdesai'}>
-                        <FaInstagram />
-                    </SocialButton>
-                </Stack>
             </Container>
         </Box>
     );
